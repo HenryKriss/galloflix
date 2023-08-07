8c4f3c5cfa821d70596aeeec9cec4d5cb07f8b45
HenryKriss <pedrowkriss@gmail.com>
2023-08-07 08:03:32 -0300

    Terminados os detalhes

--- GalloFlix/Models/Genre.cs
+++ GalloFlix/Models/Genre.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace GalloFlix.Models;
 
-    public class Genre
-    {
-       public byte Id { get; set; } 
-       public string Name { get; set; } 
-    }
+[Table("Genre")]
+public class Genre
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public byte Id { get; set; }
+    
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "O nome do Gênero é obrigatório")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Name { get; set; }    
+    public ICollection<MovieGenre> Movies { get; set; }
+}
